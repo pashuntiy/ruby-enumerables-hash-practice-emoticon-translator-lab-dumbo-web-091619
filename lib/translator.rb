00a694d241b338fe0b54b0a8d60a3bb6c1a24d2a
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-03 18:39:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,19 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(source)
+  emotions = YAML.load_file(source)
+
+  hash = {
+    "get_emoticon":  {},
+    "get_meaning": {}
+    
+  }
+
+  emotions.each do |x, y|
+    hash["get_emotions"][value[0]] = source[key][1]
+    hash["get_meaning"][value[1]] = key
+  end
+hash
 end
 
 def get_japanese_emoticon
